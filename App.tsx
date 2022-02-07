f45abfc4b0931ccabae769365b16e0bacca4d916
Saymon Araújo <67705166+saymon-araujo@users.noreply.github.com>
2022-02-07 07:46:53 -0300

    source: add README assets

--- App.tsx
+++ App.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import AppLoading from "expo-app-loading";
-import {
-  useFonts,
-  Jost_400Regular,
-  Jost_600SemiBold,
-} from "@expo-google-fonts/jost";
+import { useFonts, Jost_400Regular, Jost_600SemiBold } from "@expo-google-fonts/jost";
 
 import Routes from "./src/routes";
+
+console.disableYellowBox = true;
 
 export default function App() {
   const [fontsLoaded] = useFonts({
